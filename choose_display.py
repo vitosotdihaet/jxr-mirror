54c3350c4fabb9e513c1c0f77676d65464d8023c
VMKlimenko <vmklimenko@mai.education>
2023-03-11 17:50:47 +0300

    Handle out-of-range display ids

--- choose_display.py
+++ choose_display.py
@@ -6,6 +6,9 @@ import screeninfo as si
 
 
 monitors = si.get_monitors()
+monitor_count = len(monitors)
 
 def get_display(display_id):
+    if -monitor_count > display_id > monitor_count - 1:
+        return 0, 0
     return monitors[display_id].x, monitors[display_id].y
