63f568dd2e2e5934f45993e591b1b436eb9d6f91
Roman Ibragiomv <prime.zen2017@yandex.ru>
2023-03-12 22:14:07 +0300

    Add requirements for main.py

--- ml/main.py
+++ ml/main.py
@@ -6,7 +6,7 @@ path = input('Full image\'s path:')
 image = cv2.imread(path, 0)
 print("[INFO] Performing OCR on input image...")
 
-reader = Reader(['ru'], gpu=True)
+reader = Reader(['ru'], gpu=True) # gpu - only if you have CUDA
 results = reader.readtext(fr'{path}')
 
 for (bbox, text, prob) in results:
