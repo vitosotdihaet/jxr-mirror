1e49fa662fb9ba1d0acd88ceba6771d0c44dae64
ZendorXX <prime.zen2017@yandex.ru>
2023-03-12 22:06:14 +0300

    Add extracting text from image

---
+++ ml/main.py
@@ -0,0 +1,13 @@
+from easyocr import Reader
+import cv2
+
+path = input('Full image\'s path:')
+
+image = cv2.imread(path, 0)
+print("[INFO] Performing OCR on input image...")
+
+reader = Reader(['ru'], gpu=True)
+results = reader.readtext(fr'{path}')
+
+for (bbox, text, prob) in results:
+    print("[INFO] {:.4f}: {}".format(prob, text))
