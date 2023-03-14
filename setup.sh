caf37859d7a99e5084fe24a5a65e2a46b6d6ee0b
VMKlimenko <vmklimenko@mai.education>
2023-03-14 23:55:15 +0300

    Make separate setup files for UNIX and Windows systems

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 set -e
 
-py -3.10 -m venv .env
-source ./.env/Scripts/activate
-py -m pip install -r requirements.txt
-py -m pip uninstall opencv-python-headless -y
-py -m pip install opencv-python
+python3.10 -m venv .env
+source ./.env/bin/activate
+python -m pip install -r requirements.txt
+python -m pip uninstall opencv-python-headless -y
+python -m pip install opencv-python
 
 echo "All done!"
