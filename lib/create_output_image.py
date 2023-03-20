8c148aed3385b63012ec3a38640b92c24042446f
VMKlimenko <vmklimenko@mai.education>
2023-03-20 20:39:24 +0300

    Update create_output_image.py

--- lib/create_output_image.py
+++ lib/create_output_image.py
@@ -1,5 +1,5 @@
 '''
-This piece of code is used for generating new image from json file with medicine info
+This piece of code is used for generating new image from medicine info
 '''
 
 import cv2
@@ -7,5 +7,5 @@ import cv2
 import numpy as np
 
 
-def text_to_img(json_file):
+def text_to_img(medical_info):
     pass
